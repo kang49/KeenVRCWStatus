ff5379fd43a4366022905e5ed954fac1396c6fa4
Kankawee Aramrak <kankaweeisci123@gmail.com>
2022-10-29 23:00:50 +0700

    Update Api Key and +try func in i-o_r

--- in-out_report.py
+++ in-out_report.py
@@ -5,9 +5,9 @@ from datetime import datetime
 # VRC kAvtr Cookie
 cookies_kAvtrVRC = {
     'apiKey': 'JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26',
-    'amplitude_id_a750df50d11f21f712262cbd4c0bab37vrchat.com': 'eyJkZXZpY2VJZCI6IjY2N2VkNTVjLWVkNzMtNDk4ZS05ZDQwLTdlYzM0ODQ2MzY3MFIiLCJ1c2VySWQiOiJ1c3JfM2Y2MGYxZTctNTZjMy00MzI4LTk5YzEtMzgzZGIxMGJjNmZmIiwib3B0T3V0IjpmYWxzZSwic2Vzc2lvbklkIjoxNjY3MDU1MDY5MDU0LCJsYXN0RXZlbnRUaW1lIjoxNjY3MDU1MDg1NjIwLCJldmVudElkIjo1MCwiaWRlbnRpZnlJZCI6MCwic2VxdWVuY2VOdW1iZXIiOjUwfQ==',
-    'auth': 'authcookie_d7847060-fc69-4d03-9c09-7f7758c91456',
-    'twoFactorAuth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c3JfM2Y2MGYxZTctNTZjMy00MzI4LTk5YzEtMzgzZGIxMGJjNmZmIiwibWFjQWRkcmVzcyI6IiIsInRpbWVzdGFtcCI6MTY2NTg3MDk4MTQ4NywidmVyc2lvbiI6MSwiaWF0IjoxNjY3MDU1MDY5LCJleHAiOjE2Njk2NDcwNjksImF1ZCI6IlZSQ2hhdFR3b0ZhY3RvckF1dGgiLCJpc3MiOiJWUkNoYXQifQ.dyfvCJb644d8UIJxiuUvnABuikWhH3QjFqBjK_ivv08',
+    'auth': 'authcookie_d883b203-3742-4880-9b00-90ecfdfb4f48',
+    'twoFactorAuth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c3JfM2Y2MGYxZTctNTZjMy00MzI4LTk5YzEtMzgzZGIxMGJjNmZmIiwibWFjQWRkcmVzcyI6IiIsInRpbWVzdGFtcCI6MTY2NzA1NzUyMTYwMiwidmVyc2lvbiI6MSwiaWF0IjoxNjY3MDU3NTIxLCJleHAiOjE2Njk2NDk1MjEsImF1ZCI6IlZSQ2hhdFR3b0ZhY3RvckF1dGgiLCJpc3MiOiJWUkNoYXQifQ.figTWwc84LZ6d3QTFQD7OIzfpBLbgFFyaVa1R73ca9Y',
+    'amplitude_id_a750df50d11f21f712262cbd4c0bab37vrchat.com': 'eyJkZXZpY2VJZCI6IjQ5NzdjNjM3LTBlNDYtNDM3Yi1iNDZkLWJhMzlhZWEwMTE5MVIiLCJ1c2VySWQiOiJ1c3JfM2Y2MGYxZTctNTZjMy00MzI4LTk5YzEtMzgzZGIxMGJjNmZmIiwib3B0T3V0IjpmYWxzZSwic2Vzc2lvbklkIjoxNjY3MDU3NDk1MTk1LCJsYXN0RXZlbnRUaW1lIjoxNjY3MDU3NTQ0NDcxLCJldmVudElkIjo2LCJpZGVudGlmeUlkIjowLCJzZXF1ZW5jZU51bWJlciI6Nn0=',
 }
 
 headers_kAvtrVRC = {
@@ -25,9 +25,9 @@ headers_kAvtrVRC = {
 # VRC Nature House Cookie
 cookies_natureHouse = {
     'apiKey': 'JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26',
-    'amplitude_id_a750df50d11f21f712262cbd4c0bab37vrchat.com': 'eyJkZXZpY2VJZCI6IjY2N2VkNTVjLWVkNzMtNDk4ZS05ZDQwLTdlYzM0ODQ2MzY3MFIiLCJ1c2VySWQiOiJ1c3JfM2Y2MGYxZTctNTZjMy00MzI4LTk5YzEtMzgzZGIxMGJjNmZmIiwib3B0T3V0IjpmYWxzZSwic2Vzc2lvbklkIjoxNjY3MDU1MDY5MDU0LCJsYXN0RXZlbnRUaW1lIjoxNjY3MDU1MDg1NjIwLCJldmVudElkIjo1MCwiaWRlbnRpZnlJZCI6MCwic2VxdWVuY2VOdW1iZXIiOjUwfQ==',
-    'auth': 'authcookie_d7847060-fc69-4d03-9c09-7f7758c91456',
-    'twoFactorAuth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c3JfM2Y2MGYxZTctNTZjMy00MzI4LTk5YzEtMzgzZGIxMGJjNmZmIiwibWFjQWRkcmVzcyI6IiIsInRpbWVzdGFtcCI6MTY2NTg3MDk4MTQ4NywidmVyc2lvbiI6MSwiaWF0IjoxNjY3MDU1MDY5LCJleHAiOjE2Njk2NDcwNjksImF1ZCI6IlZSQ2hhdFR3b0ZhY3RvckF1dGgiLCJpc3MiOiJWUkNoYXQifQ.dyfvCJb644d8UIJxiuUvnABuikWhH3QjFqBjK_ivv08',
+    'auth': 'authcookie_d883b203-3742-4880-9b00-90ecfdfb4f48',
+    'twoFactorAuth': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c3JfM2Y2MGYxZTctNTZjMy00MzI4LTk5YzEtMzgzZGIxMGJjNmZmIiwibWFjQWRkcmVzcyI6IiIsInRpbWVzdGFtcCI6MTY2NzA1NzUyMTYwMiwidmVyc2lvbiI6MSwiaWF0IjoxNjY3MDU3NTIxLCJleHAiOjE2Njk2NDk1MjEsImF1ZCI6IlZSQ2hhdFR3b0ZhY3RvckF1dGgiLCJpc3MiOiJWUkNoYXQifQ.figTWwc84LZ6d3QTFQD7OIzfpBLbgFFyaVa1R73ca9Y',
+    'amplitude_id_a750df50d11f21f712262cbd4c0bab37vrchat.com': 'eyJkZXZpY2VJZCI6IjQ5NzdjNjM3LTBlNDYtNDM3Yi1iNDZkLWJhMzlhZWEwMTE5MVIiLCJ1c2VySWQiOiJ1c3JfM2Y2MGYxZTctNTZjMy00MzI4LTk5YzEtMzgzZGIxMGJjNmZmIiwib3B0T3V0IjpmYWxzZSwic2Vzc2lvbklkIjoxNjY3MDU3NDk1MTk1LCJsYXN0RXZlbnRUaW1lIjoxNjY3MDU3NTQ0NDcxLCJldmVudElkIjo2LCJpZGVudGlmeUlkIjowLCJzZXF1ZW5jZU51bWJlciI6Nn0=',
 }
 
 headers_natureHouse = {
@@ -47,30 +47,33 @@ favNow_kAvtr = 0
 playersNow_natureHouse = 0
 favNow_natureHouse = 0
 while True:
-    kAvtrData = requests.get('https://vrchat.com/api/1/worlds/wrld_e26b7a14-e8cf-4a0f-890f-e5676bd40032', cookies=cookies_kAvtrVRC, headers=headers_kAvtrVRC)
-    kAvtrData = kAvtrData.json()
+    try:
+        kAvtrData = requests.get('https://vrchat.com/api/1/worlds/wrld_e26b7a14-e8cf-4a0f-890f-e5676bd40032', cookies=cookies_kAvtrVRC, headers=headers_kAvtrVRC)
+        kAvtrData = kAvtrData.json()
 
-    natureHouseData = requests.get('https://vrchat.com/api/1/worlds/wrld_6e447f3a-c9c0-49b4-a5bf-b098b84fb585', cookies=cookies_natureHouse, headers=headers_natureHouse)
-    natureHouseData = natureHouseData.json()
+        natureHouseData = requests.get('https://vrchat.com/api/1/worlds/wrld_6e447f3a-c9c0-49b4-a5bf-b098b84fb585', cookies=cookies_natureHouse, headers=headers_natureHouse)
+        natureHouseData = natureHouseData.json()
 
-    if kAvtrData['occupants'] != playersNow_kAvtr:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print(current_time, "Keen's Avatars World" ,'Now players is', kAvtrData['occupants'] , 'People')
-        playersNow_kAvtr = kAvtrData['occupants']
-    if kAvtrData['favorites'] != favNow_kAvtr:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print(current_time, "Keen's Avatars World" , 'Have' , kAvtrData['favorites'] , 'Favorites')
-        favNow_kAvtr = kAvtrData['favorites']
-    
-    if natureHouseData['occupants'] != playersNow_natureHouse:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print(current_time, "Nature House" , 'Now players is' , natureHouseData['occupants'] , 'People')
-        playersNow_natureHouse = natureHouseData['occupants']
-    if natureHouseData['favorites'] != favNow_natureHouse:
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print(current_time, "Nature House" , 'Have' , natureHouseData['favorites'] , 'Favorites')
-        favNow_natureHouse = natureHouseData['favorites']
+        if kAvtrData['occupants'] != playersNow_kAvtr:
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            print(current_time, "Keen's Avatars World" ,'Now players is', kAvtrData['occupants'] , 'People')
+            playersNow_kAvtr = kAvtrData['occupants']
+        if kAvtrData['favorites'] != favNow_kAvtr:
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            print(current_time, "Keen's Avatars World" , 'Have' , kAvtrData['favorites'] , 'Favorites')
+            favNow_kAvtr = kAvtrData['favorites']
+        
+        if natureHouseData['occupants'] != playersNow_natureHouse:
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            print(current_time, "Nature House" , 'Now players is' , natureHouseData['occupants'] , 'People')
+            playersNow_natureHouse = natureHouseData['occupants']
+        if natureHouseData['favorites'] != favNow_natureHouse:
+            now = datetime.now()
+            current_time = now.strftime("%H:%M:%S")
+            print(current_time, "Nature House" , 'Have' , natureHouseData['favorites'] , 'Favorites')
+            favNow_natureHouse = natureHouseData['favorites']
+    except:
+        print('API Key Expired')
